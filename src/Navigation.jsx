8a79e33c7cea564365d1ad2597b23cc8b7667fd7
chinmay <chinmaydeshpande2018@gmail.com>
2025-05-22 22:00:50 -0500

    Navigation Bar

---
+++ src/Navigation.jsx
@@ -0,0 +1,13 @@
+import './Navigation.css'
+function Navigation() {
+  return (
+    <div className="navigation">
+      <div className="nav-item">Home</div>
+      <div className="nav-item">About Me </div>
+      <div className="nav-item">Work Experience</div>
+      <div className="nav-item">Projects</div>
+      <div className="nav-item">Contacts</div>
+    </div>
+  );
+}
+export default Navigation
