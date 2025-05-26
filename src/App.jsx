ef26982aa029278dd4b16023d62898af78734bfc
chinmay <chinmaydeshpande2018@gmail.com>
2025-05-26 18:25:24 -0500

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navigation from './Navigation.jsx'
+import Bar from './Bar.jsx'
 function App() {
 
   return (
-    <>
+    <div>
       <Navigation/>
-    </>
-  )
+      <Bar/>
+    </div>
+  );
 }
 
 export default App
---
+++ src/Bar.jsx
@@ -0,0 +1,6 @@
+function Bar() {
+  return (
+    <H1>THis is Text.</H1>
+  );
+}
+export default Bar
